00d761b9544d76d9b440e387deade4823c7125ac
shivamsk2004 <shivamkumbhakarna2004@gmail.com>
2025-03-01 11:24:32 +0530

    NextJS initialised

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Subscribe to Tube</h2>
+      <Button variant="destructive">Subscribe</Button>
+      <button>Subscribe</button>
+    </div>
+  );
+}
