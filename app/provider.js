ac23ff70bee7c486fa98ee60d3306bdb86e7fa0e
shivamsk2004 <shivamkumbhakarna2004@gmail.com>
2025-03-01 17:25:28 +0530

    Add a user to db

--- app/provider.js
+++ app/provider.js
@@ -1,11 +1,31 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+import {db} from '@/configs/db';
+import {Users} from '@/configs/schema';
+import { useUser } from '@clerk/nextjs';
+import { eq } from 'drizzle-orm';
 
-const Provider = ({children}) => {
-  return (
-    <div>
-    {children}
-    </div>
-  )
+function Provider({children}){
+    const {user}=useUser();
+
+    useEffect(()=>{
+        user&&isNewUser();
+    },[user]);
+
+    const isNewUser=async()=>{
+        const result=await db.select().from(Users)
+        .where(eq(Users.email,user?.primaryEmailAddress?.emailAddress));
+        
+        console.log(result);
+        if(!result[0])
+        {
+            await db.insert(Users).values({
+                name:user.fullName,
+                email:user?.primaryEmailAddress?.emailAddress,
+                imageUrl:user?.imageUrl
+            })
+        }
+    }
 }
 
 export default Provider
