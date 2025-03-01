a23c6b6919d8dd44313b91b1af618d0317aa34be
shivamsk2004 <shivamkumbhakarna2004@gmail.com>
2025-03-01 17:14:55 +0530

    Authentication Using Clerk

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,7 +7,7 @@ export default function Home() {
     <div>
       <h2>Subscribe to Tube</h2>
       <Button variant="destructive">Subscribe</Button>
-      <button>Subscribe</button>
+      <UserButton/>
     </div>
   );
 }
